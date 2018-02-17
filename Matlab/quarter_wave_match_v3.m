a1c9a053baf5287010f1c143eaa5edfff2a351e7
Eric Brown <brown.eric.m@gmail.com>
2018-02-17 00:11:13 -0500

    Updated matching section
    This should be the final change. I have found that m = 1:4 are the only
    viable multiples of 2pi if we maintain a constant height. Past that, the
    dielectric difference btw one layer and another change too fast. I tried
    to solve for eps_r but the quadratic was extraordinarily hairy.

--- Matlab/quarter_wave_match_v3.m
+++ Matlab/quarter_wave_match_v3.m
@@ -14,20 +14,37 @@ k0 = (2*pi)/lam0;
 N = 4;
 er = linspace(1,10,100);
 er = er';
-m = 1:10;
+m_max = 10;
 n = 1:4;
 
-%delt = 
-%h_p = h + 2*delt;
 
 %% setting up vectors
-
-%end
 er_mat = repmat( er, size(n));
 n_mat = repmat(n, 100, 1);
-%m_mat = repmat(n, 100, 1);
 
-%plot(eps_r,y);
-%h_1 = (lam0/2)*er_mat.^(1/4);
-%h_2 = (2*pi.*n.*m)/N + pi;
-h = ((lam0/2)*er_mat.^(1/4)+(2*pi.*n_mat*1)/N + pi)./(k0*sqrt(er_mat));
+h.m = struct('v',{});
+for m = 1:10
+    h.m{m} = (k0*(lam0/2)*er_mat.^(1/4)+k0*(lam0*n_mat)/4+(2*pi.*n_mat*m)/N + pi)./(k0*sqrt(er_mat));
+end
+
+%% Plots visualizing h vs er_n at various multiples of m
+
+figure
+plot(h.m{10}, er_mat)
+legend('n = 1','n = 2','n = 3','n = 4')
+xlabel('Total height h = h'' + 2\delta')
+ylabel('\bf\epsilon_r')
+er_n = [9.727, 6.182, 3.364, 1.273];
+%% Calculate structure height
+% h_p = h + 2*delt
+for ii = n
+    delt(ii) = lam0/(2*er_n(ii)^(1/4));
+end
+h_p = .06*ones(1,4) - 2.*(delt);
+
+
+%% Other way
+% 
+% for ii = n
+%     er_a(ii) = (((ii*pi)/(4*lam0)-(2*pi*4*ii)/4+pi)./(k0.*.039)).^2;
+% end
