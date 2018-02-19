81307ed7292449d5d08ed742bfdc70a8013fbc78
Eric Brown <brown.eric.m@gmail.com>
2018-02-19 07:51:56 -0500

    added functionality to get er at specific height
    added 3 lines that pulls the index of a specific total height and gets
    the er at that index to get the height of the lens and matching layers
    as a 1xN vector

--- Matlab/quarter_wave_match_v3.m
+++ Matlab/quarter_wave_match_v3.m
@@ -1,18 +1,26 @@
 clear all
 close all
 %% Eqs
-% k0*(n/N)*sin(theta)+(n*2*pi)/N = k0*sqrt(eps_r(n))*h_p
+% k0*(n/N)*sin(theta)+(n*m*2*pi)/N - pi = k0*sqrt(eps_r(n))*(h-2delt)
+% where 
+% n = is a slice of constant er
+% N = number of slices per cell
+% m = multiples of 2pi
+% h = height of lens + 2*matching layers
+% sin(theta) = angle off zenith
+% LHS pi = decribes the phase effect of matching layer (180deg)
+
 
 %%  CONSTANTS
 eps_0 = 8.85*10^(-12); %permittivity
 eps_air = 1.0006;
 mu_0 = 1; %permeability
 c = 299792458;
-f = 10e9;
+f = 5.8e9;
 lam0 = c/f;
 k0 = (2*pi)/lam0;
 N = 4;
-er = linspace(1,10,100);
+er = linspace(1,10,1000);
 er = er';
 m_max = 10;
 n = 1:4;
@@ -20,27 +28,46 @@ n = 1:4;
 
 %% setting up vectors
 er_mat = repmat( er, size(n));
-n_mat = repmat(n, 100, 1);
+n_mat = repmat(n, 1000, 1);
 
 h.m = struct('v',{});
-for m = 1:10
-    h.m{m} = (k0*(lam0/2)*er_mat.^(1/4)+k0*(lam0*n_mat)/4+(2*pi.*n_mat*m)/N + pi)./(k0*sqrt(er_mat));
+% Q for KC: Can the pi for the matching layer be pos or neg? if only pos,
+% then there is no suitable solution w/in out er values
+for m = 0:m_max
+    h.m{m+1} = (k0*(lam0/2)*er_mat.^(1/4)+k0*(lam0*n_mat)/4+(2*pi.*n_mat*m)/N + 3*pi)./(k0*sqrt(er_mat));
 end
 
+% for m = 0:m_max
+%     h.m{m+1} = (k0*(lam0/2)*er_mat.^(1/4)+k0*(lam0*n_mat)/4+(2*pi.*n_mat*m)/N + pi)./(k0*sqrt(er_mat));
+% end
 %% Plots visualizing h vs er_n at various multiples of m
 
 figure
-plot(h.m{10}, er_mat)
+plot(h.m{1}, er_mat)
 legend('n = 1','n = 2','n = 3','n = 4')
 xlabel('Total height h = h'' + 2\delta')
 ylabel('\bf\epsilon_r')
-er_n = [9.727, 6.182, 3.364, 1.273];
+%% Find ern from given height
+% I'm sure there is a way to get an optimal solution for this
+% but this is how to get er_n by visually inspecting graph
+% and selecting a reasonable structure height
+
+%EXAMPLE: THIS GETS er_n FOR 4cm, at m = 0
+h_t = .07;
+tmp = abs(h.m{1}-0.07);
+[~,index] = min(tmp);
+er_n = er_mat(index);
+er_m = sqrt(er_n);
+
 %% Calculate structure height
-% h_p = h + 2*delt
+% h = total height
+% delt = height of the matching layers (2)
+% h_p = height of the lens sans matching layer
+delt = zeros(1,4);
 for ii = n
     delt(ii) = lam0/(2*er_n(ii)^(1/4));
 end
-h_p = .06*ones(1,4) - 2.*(delt);
+h_p = h_t*ones(1,4) - 2.*(delt);
 
 
 %% Other way
